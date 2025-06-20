a80075b19c5e55968bec84e60316b7f7b8a3acf3
Asuka-Aozora <azdin1411@gmail.com>
2025-06-20 08:04:43 +0700

    feat(sales): change update order status and subscription routes to use PATCH method; enhance getOrder to handle pagination

--- modules/sejoli/sales/salesController.js
+++ modules/sejoli/sales/salesController.js
@@ -2,7 +2,7 @@ const m = require("./salesModel");
 const fn = require("../../../common/fn");
 
 exports.getOrder = async (req, res) => {
-  let dt = { err: false, msg: "", flow: [], code: 200 };
+  let dt = { err: false, msg: "", flow: [], code: 200, req_query: req.query };
   dt = await m.getOrder(dt);
   console.log("dt ", dt);
   res.status(dt.code).json(fn.setResponse(dt));
