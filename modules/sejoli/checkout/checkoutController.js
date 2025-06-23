257e55fe7b1cb63704e600b9ec990b57bb7bc28a
Asuka-Aozora <azdin1411@gmail.com>
2025-06-23 13:45:17 +0700

    feat: add php-serialize package for PHP serialization in JavaScript
    - Added php-serialize as a dependency in package.json.
    - Included license and README files for php-serialize.
    - Implemented core serialization and unserialization logic in the library.
    - Added typings for TypeScript support.
    - Updated package.json with necessary scripts and metadata.

--- modules/sejoli/checkout/checkoutController.js
+++ modules/sejoli/checkout/checkoutController.js
@@ -1,9 +1,16 @@
 const m = require("./checkoutModel");
 const fn = require("../../../common/fn");
 
-exports.getOrder = async (req, res) => {
+exports.getCK = async (req, res) => {
   let dt = { err: false, msg: "", flow: [], code: 200 };
-  dt = await m.getOrder(dt);
+  dt = await m.getCK(dt);
+  console.log("dt ", dt);
+  res.status(dt.code).json(fn.setResponse(dt));
+};
+
+exports.getCP = async (req, res) => {
+  let dt = { err: false, msg: "", flow: [], code: 200 };
+  dt = await m.getCP(dt);
   console.log("dt ", dt);
   res.status(dt.code).json(fn.setResponse(dt));
 };
--- modules/sejoli/checkout/checkoutModel.js
+++ modules/sejoli/checkout/checkoutModel.js
@@ -1,17 +1,19 @@
 const fn = require('../../../common/fn');
+const unserialize = require("php-serialize").unserialize;
 
-exports.getOrder = async (dt) => {
-     if (dt.err) {dt.flow.push('❌ salesModel.js | bypass getOrder');return dt;}
-        dt.flow.push('➡️. salesModel.js | start getOrder');
+exports.getCK = async (dt) => {
+     if (dt.err) {dt.flow.push('❌ salesModel.js | bypass getCK');return dt;}
+        dt.flow.push('➡️. salesModel.js | start getCK');
 
         let rows=[];
         
         try {
             [rows] = await fn.db.query(`
-            SELECT o.*, u.display_name, u.user_email, p.post_title AS product_name
+            SELECT o.*, u.display_name, u.user_email, p.post_title AS product_name, c.*
             FROM wp_sejolisa_orders o
             JOIN wp_users u ON o.user_id = u.ID
             JOIN wp_posts p ON o.product_id = p.ID
+            JOIN wp_sejolisa_coupons c
         `);
         } catch (error) {
             dt.flow.push('❌ salesModel.js | Error querying database. '+error);
@@ -39,6 +41,53 @@ exports.getOrder = async (dt) => {
 
         return dt;
 }
+
+exports.getCP = async (dt) => {
+  if (dt.err) {
+    dt.flow.push("❌ salesModel.js | bypass getCP");
+    return dt;
+  }
+  dt.flow.push("➡️. salesModel.js | start getCP");
+
+  let rows = [];
+
+  try {
+    [rows] = await fn.db.query(`
+         SELECT c.*
+         FROM wp_sejolisa_coupons c
+     `);
+  } catch (error) {
+    dt.flow.push("❌ salesModel.js | Error querying database. " + error);
+    dt.err = true;
+    dt.code = 500;
+    return dt;
+  }
+
+  let arr = [];
+  if (rows && rows.length > 0) {
+    // console.log(`✅ salesModel.js | Ditemukan ${rows.length} data.`);
+    rows.forEach((row) => {
+      try {
+        const parsedDiscount = unserialize(row.discount);
+        row.discount = parsedDiscount;
+      } catch (e) {
+        row.discount = null;
+      }
+      arr.push(row);
+    });
+  } else {
+    dt.flow.push("❌ salesModel.js | Tidak ada data ditemukan di DB.");
+    dt.err = true;
+    dt.code = 404;
+    return dt;
+  }
+
+  dt.data = arr;
+  dt.flow.push("✅ salesModel.js | data orders found");
+  dt.err = false;
+
+  return dt;
+};
 
 exports.postCheckout = async (dt) => {
   const { product_id, quantity, username, email, phone, payment_gateway, status } = dt.req_body;
