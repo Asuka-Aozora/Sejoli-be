7f9049eaab40892e034f58adfa3f8b5fe04120fd
Asuka-Aozora <azdin1411@gmail.com>
2025-06-30 11:06:25 +0700

    feat(checkout): add updateQuantity endpoint; implement stock update logic for checkout

--- modules/sejoli/checkout/checkoutController.js
+++ modules/sejoli/checkout/checkoutController.js
@@ -44,14 +44,45 @@ exports.getCK = async (req, res) => {
   res.status(dt.code).json(fn.setResponse(dt));
 };
 
-exports.postCheckout = async (req,res) => {
-    let dt = {err:false,msg:'',flow:[],code:200,req_body:req.body,}
-    dt = await m.postCheckout(dt);
-    res.status(dt.code).json({
-      code: dt.code,
-      err: dt.err,
-      msg: dt.msg,
-      flow: dt.flow,
-      data: dt.req_body,
-    });;
+exports.postCheckout = async (req, res) => {
+  let dt = { err: false, msg: "", flow: [], code: 200, req_body: req.body };
+  dt = await m.postCheckout(dt);
+  res.status(dt.code).json({
+    code: dt.code,
+    err: dt.err,
+    msg: dt.msg,
+    flow: dt.flow,
+    data: dt.req_body,
+  });
+};
+
+exports.updateQuantity = async (req, res) => {
+  // Inisialisasi DT object sesuai konvensi
+  let dt = {
+    err: false,
+    msg: "",
+    flow: [],
+    code: 200,
+    req_body: req.body,
+  };
+
+  // Panggil model updateQuantity
+  dt = await m.updateQuantity(dt);
+
+  // Jika model menandakan error, ubah HTTP status
+  if (dt.err) {
+    dt.code = 400;
+    dt.msg = dt.flow[dt.flow.length - 1] || "Error updating quantity";
+  } else {
+    dt.msg = "Quantity updated successfully";
+  }
+
+  // Kirim response
+  return res.status(dt.code).json({
+    code: dt.code,
+    err: dt.err,
+    msg: dt.msg,
+    flow: dt.flow,
+    data: dt.data, // data detail perubahan stok
+  });
 };
